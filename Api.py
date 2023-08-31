bf5c21105d662d52001878edba4787a6079b8906
LeonovGeorgy <leonovegor94@outlook.com>
2023-08-31 11:07:25 +0300

    Initial commit

--- Api.py
+++ Api.py
@@ -7,34 +7,45 @@ api = Api()
 films = {
     1: {"film": "Avatar", "duration": 2.7},
     2: {"film": "Alien", "duration": 1.8},
-
+}
+actors = {
+    1: {'name': 'LeoDiCaprio', 'age': '48'},
+    2: {'name': 'MarkRuffalo', 'age': '55'}
 }
 
 parser = reqparse.RequestParser()      # описываем в каком виде получаем инфо по фильмам
 parser.add_argument("film", type=str)
 parser.add_argument("duration", type=float)
 
-class Main(Resource):
-    def get(self, films_id):
-        if films_id == 0:
+class Main(Resource):    # клас по управлению сайтом
+    def get(self, film_id):
+        if film_id == 0:
             return films
         else:
-            return films[films_id]
+            return films[film_id]
 
-    def delete(self, films_id):
-        del films[films_id]
+    def delete(self, film_id):
+        del films[film_id]
         return films
 
-    def post(self, films_id):
-        films[films_id] = parser.parse_args()
+    def post(self, film_id):
+        films[film_id] = parser.parse_args()
         return films
 
-    def put(self, films_id):
-        films[films_id] = parser.parse_args()
+    def put(self, film_id):
+        films[film_id] = parser.parse_args()
         return films
 
-api.add_resource(Main, "/api/films/<int:films_id>")
-api.init_app(app)
+    def get(self, actor_id):
+        if actor_id == 0:
+            return actors
+        else:
+            return actors[actor_id]
+
+if api.add_resource(Main, "/api/films/<int:film_id>"):
+    api.init_app(app)
+elif api.add_resource(Main, "/api/actors/<int:actor_id>"):
+    api.init_app(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- local.py
+++ local.py
@@ -1,9 +1,10 @@
 import requests
 
 # Выполняя запрос ниже добовляем еще один фмльм
-res = requests.post("http://127.0.0.1:5000/api/films/3", json={"film": "Terminator", "duration": 1.5})
+#res = requests.post("http://127.0.0.1:5000/api/films/3", json={"film": "Terminator", "duration": 1.5})
 
+# Получить список актеров
 
-
+res = requests.get("http://127.0.0.1:5000/api/films/1")
 
 print(res.json())
