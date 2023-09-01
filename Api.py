ad6cec103703754d47d689fb887749706b5b9dd1
LeonovGeorgy <leonovegor94@outlook.com>
2023-09-01 10:19:54 +0300

    В локал еще команды пропишу, чтобы баловаться можно было и го

--- Api.py
+++ Api.py
@@ -4,13 +4,12 @@ from flask_restful import Api, Resource, reqparse
 app = Flask(__name__)
 api = Api()
 
+# Как бы наш сайт.
+# В файле локал пропишем команды, как если бы нажимали на ссылки.
 films = {
-    1: {"film": "Avatar", "duration": 2.7},
-    2: {"film": "Alien", "duration": 1.8},
-}
-actors = {
-    1: {'name': 'LeoDiCaprio', 'age': '48'},
-    2: {'name': 'MarkRuffalo', 'age': '55'}
+    1: {"film": "Batman Begins", "duration_min": 140},
+    2: {"country": "USA", "fees_mill$": 205},
+    3: {'Christian Bale': 'Batman', 'Michael Caine': 'Alfred'}
 }
 
 parser = reqparse.RequestParser()      # описываем в каком виде получаем инфо по фильмам
@@ -18,34 +17,28 @@ parser.add_argument("film", type=str)
 parser.add_argument("duration", type=float)
 
 class Main(Resource):    # клас по управлению сайтом
-    def get(self, film_id):
+    def get(self, film_id):         # получаем инфо по ссылке
         if film_id == 0:
             return films
         else:
             return films[film_id]
 
-    def delete(self, film_id):
+    def delete(self, film_id):      # удалить ссылку
         del films[film_id]
         return films
 
-    def post(self, film_id):
+    def post(self, film_id):        # добавить новую ссылку
         films[film_id] = parser.parse_args()
         return films
 
-    def put(self, film_id):
+    def put(self, film_id):         # обновить имеющуюся
         films[film_id] = parser.parse_args()
         return films
 
-    def get(self, actor_id):
-        if actor_id == 0:
-            return actors
-        else:
-            return actors[actor_id]
 
-if api.add_resource(Main, "/api/films/<int:film_id>"):
-    api.init_app(app)
-elif api.add_resource(Main, "/api/actors/<int:actor_id>"):
-    api.init_app(app)
+api.add_resource(Main, "/api/films/<int:film_id>")
+api.init_app(app)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- local.py
+++ local.py
@@ -3,8 +3,12 @@ import requests
 # Выполняя запрос ниже добовляем еще один фмльм
 #res = requests.post("http://127.0.0.1:5000/api/films/3", json={"film": "Terminator", "duration": 1.5})
 
-# Получить список актеров
+# Получить инфо по фильму
+#res = requests.get("http://127.0.0.1:5000/api/films/1")
 
-res = requests.get("http://127.0.0.1:5000/api/films/1")
+# Получить инфо по фильму
+#res = requests.get("http://127.0.0.1:5000/api/films/1")
 
+# Получить инфо по фильму
+#res = requests.get("http://127.0.0.1:5000/api/films/1")
 print(res.json())
