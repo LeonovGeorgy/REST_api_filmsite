963fddbe3b4287d795c4a8fe0782c0e51c47a803
LeonovGeorgy <leonovegor94@outlook.com>
2023-09-01 11:11:03 +0300

    дописал в апи условия по страницам и в локал добавил еще команд

--- Api.py
+++ Api.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
@@ -12,31 +12,52 @@ films = {
     3: {'Christian Bale': 'Batman', 'Michael Caine': 'Alfred'}
 }
 
-parser = reqparse.RequestParser()      # описываем в каком виде получаем инфо по фильмам
+parser = reqparse.RequestParser()# описываем в каком виде получаем инфо по фильмам
 parser.add_argument("film", type=str)
-parser.add_argument("duration", type=float)
+parser.add_argument("duration_min", type=int)
+parser.add_argument("country", type=str)
+parser.add_argument("fees_mill$", type=float)
 
 class Main(Resource):    # клас по управлению сайтом
-    def get(self, film_id):         # получаем инфо по ссылке
-        if film_id == 0:
+    def get(self, site_id):         # получаем инфо по ссылке
+        if site_id == 0:
             return films
         else:
-            return films[film_id]
+            return films[site_id]
 
-    def delete(self, film_id):      # удалить ссылку
-        del films[film_id]
+    def delete(self, site_id):      # удалить ссылку
+        del films[site_id]
         return films
 
-    def post(self, film_id):        # добавить новую ссылку
-        films[film_id] = parser.parse_args()
+    #def post(self, site_id):        # добавить новую ссылку
+        #films[site_id] = parser.parse_args()
+        #return films
+    def post(self, site_id):
+        data = parser.parse_args()
+        if site_id not in films:
+            films[site_id] = {}
+        if data["film"]:
+            films[site_id]["film"] = data["film"]
+            films[site_id]["duration_min"] = data["duration_min"]
+        elif data["country"]:
+            films[site_id]["country"] = data["country"]
+            films[site_id]["fees_mill$"] = data["fees_mill$"]
         return films
 
-    def put(self, film_id):         # обновить имеющуюся
-        films[film_id] = parser.parse_args()
+    def put(self, site_id):         # обновить имеющуюся
+        data = parser.parse_args()
+        if site_id not in films:
+            films[site_id] = {}
+        if data["film"]:
+            films[site_id]["film"] = data["film"]
+            films[site_id]["duration_min"] = data["duration_min"]
+        elif data["country"]:
+            films[site_id]["country"] = data["country"]
+            films[site_id]["fees_mill$"] = data["fees_mill$"]
         return films
 
 
-api.add_resource(Main, "/api/films/<int:film_id>")
+api.add_resource(Main, "/api/films/<int:site_id>")
 api.init_app(app)
 
 
--- local.py
+++ local.py
@@ -1,14 +1,17 @@
 import requests
 
-# Выполняя запрос ниже добовляем еще один фмльм
-#res = requests.post("http://127.0.0.1:5000/api/films/3", json={"film": "Terminator", "duration": 1.5})
-
 # Получить инфо по фильму
 #res = requests.get("http://127.0.0.1:5000/api/films/1")
 
-# Получить инфо по фильму
-#res = requests.get("http://127.0.0.1:5000/api/films/1")
+# Получить инфо по сборам
+#res = requests.get("http://127.0.0.1:5000/api/films/2")
 
-# Получить инфо по фильму
-#res = requests.get("http://127.0.0.1:5000/api/films/1")
+# Получить инфо по актёрскому составу
+#res = requests.get("http://127.0.0.1:5000/api/films/3")
+
+# Добавляем инфо по сборам в России
+#res = requests.post("http://127.0.0.1:5000/api/films/4", json={"country": "Rus", "fees_mill$": 1.3})
+
+# Добавляем инфо по новому фильму
+res = requests.post("http://127.0.0.1:5000/api/films/5", json={"film": "The Dark Knight", "duration_min": 152})
 print(res.json())
